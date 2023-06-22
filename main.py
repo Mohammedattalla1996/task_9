1739daf88a6ab757170aa5d20d18e181f9b7393a
Mohammedattalla1996 <attallamohammed46@gmail.com>
2023-06-22 20:14:11 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+    def area(self):
+        return self.length * self.width
+
+    def display(self):
+        print("Length:", self.length)
+        print("Width:", self.width)
+        print("Perimeter:", self.perimeter())
+        print("Area:", self.area())
+
+
+class Parallelepipede(Rectangle):
+    def __init__(self, length, width, height):
+        super().__init__(length, width)
+        self.height = height
+
+    def volume(self):
+        return self.length * self.width * self.height
+
+
+rectangle = Rectangle(6, 4)
+rectangle.display()
